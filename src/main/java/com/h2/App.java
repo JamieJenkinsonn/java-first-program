c043c05e9fdf375ad0ed052c1aab04fd842af526
JamieJenkinsonn <99878909+JamieJenkinsonn@users.noreply.github.com>
2022-12-19 18:19:49 +0000

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,10 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return 2*(-1);
+        return 2*(1);
+    }
+
+    private static int add(int[] numbers) {
+        return (-1);
     }
 }
